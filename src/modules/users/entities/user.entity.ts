c297ca9a8c0bdf457a74bc538dffbe70a1436035
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2026-05-08 13:38:03 +0100

    fix(auth): remove ApiPropterty() decorator from user entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
 import { Column, Entity, Index } from 'typeorm';
 import { AbstractBaseEntity } from '../../../database/entities/abstract-base.entity';
@@ -6,7 +5,6 @@ import { UserRole } from '../../../common/enums';
 
 @Entity('users')
 export class User extends AbstractBaseEntity {
-  @ApiProperty({ example: 'user@example.com' })
   @Index({ unique: true })
   @Column({ type: 'varchar', length: 255, unique: true })
   email: string;
@@ -15,43 +13,33 @@ export class User extends AbstractBaseEntity {
   @Column({ type: 'varchar', length: 255 })
   passwordHash: string;
 
-  @ApiProperty()
   @Column({ type: 'varchar', length: 255 })
   firstName: string;
 
-  @ApiProperty()
   @Column({ type: 'varchar', length: 255 })
   lastName: string;
 
-  @ApiProperty()
   @Column({ type: 'varchar', length: 255, nullable: true })
   googleId?: string;
 
-  @ApiProperty()
   @Column({ type: 'boolean', nullable: true, default: false })
   emailVerified: boolean;
 
-  @ApiProperty()
   @Column({ type: 'varchar', length: 30, nullable: true })
   inverterBrand?: string;
 
-  @ApiProperty()
   @Column({ type: 'smallint', nullable: true })
   onboardingStep?: number;
 
-  @ApiProperty()
   @Column({ type: 'boolean', nullable: true })
   onboardingComplete?: boolean;
 
-  @ApiProperty()
   @Column({ type: 'boolean', nullable: true, default: false })
   isActive?: boolean;
 
-  @ApiProperty()
   @Column({ type: 'timestamptz', nullable: true })
   lastLoginAt?: Date;
 
-  @ApiProperty({ enum: UserRole, default: UserRole.USER })
   @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
   role: UserRole;
 
